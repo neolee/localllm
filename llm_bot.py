f483cb676af7f661d945dd59250143d18effb2cd
Neo Lee <neo.lee@gmail.com>
2024-05-14 21:21:53 +0800

    refactor: separate api config into module

--- llm_bot.py
+++ llm_bot.py
@@ -1,32 +1,5 @@
-from openai import OpenAI
-
 from bot import Bot
-
-
-# init client point to the local server, api_key is irrelevant
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-model_name = "NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF"
-
-system_message_file = "system_message.txt"
-
-user_name = "Neo"
-agent_name = "Garfield"
-
-def load_system_message():
-    newline = '\n'
-    with open(system_message_file, 'r') as f:
-        system_message = f"{f.read().replace(newline, ' ')}".format(**globals())
-    return system_message
-
-def create_chat_completion(q, sm):
-    return client.chat.completions.create(
-        model=model_name,
-        messages=[
-            {"role": "system", "content": sm},
-            {"role": "user", "content": q}
-        ],
-        temperature=0.7,
-    )
+from api import load_system_message, create_chat_completion
 
 
 class LLMBot(Bot):
