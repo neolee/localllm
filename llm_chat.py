ff0b7c8ce5f0d352a4380f4f1f37daddc59425e1
Neo Lee <neo.lee@gmail.com>
2024-05-14 20:09:19 +0800

    feat: more reusable and practical

--- llm_chat.py
+++ llm_chat.py
@@ -1,16 +1,46 @@
-# Example: reuse your existing OpenAI setup
 from openai import OpenAI
 
-# Point to the local server
+# init client point to the local server, api_key is irrelevant
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
-completion = client.chat.completions.create(
-  model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
-  messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
-  ],
-  temperature=0.7,
-)
+user_name = "Neo"
+agent_name = "Garfield"
 
-print(completion.choices[0].message)
+
+def create_chat_completion(q, sm):
+    return client.chat.completions.create(
+        model="NousResearch/Hermes-2-Pro-Llama-3-8B-GGUF",
+        messages=[
+            {"role": "system", "content": sm},
+            {"role": "user", "content": q}
+        ],
+        temperature=0.7,
+    )
+
+
+def main():
+    # system_message = "Perform the task to the best of your ability."
+    system_message = f"""
+        "The user name is {user_name}. You are the assistant or chatbot and your name is {agent_name}. "
+        "Always refer to the user as {user_name}. Keep all responses brief and concise. "
+        "You, the assistant, are an expert in Artificial Intelligence, Machine Learning, Deep Learning, Generative AI, "
+        "Large Language Models, Transformers, Open Source LLMs, computer Science and Math.\n"
+        "Your primary job and role as the ASSISTANT is to help {user_name} learn, design, code and deploy his personal AI assistant. "
+        "We will accomplish this job by using open source LLMs, python libraries, TTS, STT, and Hugging Face and GitHub tools and resources.\n"
+        "Be concise and specific in responses. Avoid unnecessary details. Role-play accurately, understanding and mirroring user intent during scenarios.\n"
+        "Emphasize honesty, candor, and precision. Avoid speculation except when explicitly prompted to. "
+        "Maintain a friendly respectful and professional tone. Politely correct me if I am wrong and give evidence based facts. Never lecture me."
+    """
+
+    while True:
+        q = input(f"{user_name}: ")
+        if q.lower() in ['exit', 'bye', 'quit', 'x', 'q']:
+            print("exiting the chat.")
+            break
+
+        completion = create_chat_completion(q, system_message)
+        print(f"{agent_name}: ", completion.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
