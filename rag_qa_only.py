ce7a3b7c8393b54c76202f808bac74d2f4ea5391
Neo Lee <neo.lee@gmail.com>
2024-05-15 00:48:38 +0800

    feat: RetrievalQA -> create_retrieval_chain

--- rag_qa_only.py
+++ rag_qa_only.py
@@ -1,9 +1,10 @@
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
 from langchain_core.utils.utils import convert_to_secret_str
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -15,7 +16,7 @@ vector_db = Chroma(persist_directory="./chroma_db", embedding_function=embedding
 
 query = "What is the CAP theorem?"
 
-print("\nSearching for similar documents to:", query, "\n")
+print("> searching for similar documents to:", query, "\n")
 
 # search and show the result
 search_results = vector_db.similarity_search(query, k=2)
@@ -30,20 +31,26 @@ llm = ChatOpenAI(temperature=0.5,
                  api_key=convert_to_secret_str("lm-studio"))
 
 # build prompt
-template = """Use the following pieces of context to answer the question at the end. \
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. \
-    Use three sentences maximum. {context} \
-    Question: {question}
-    Helpful Answer:"""
-qa_chain_prompt = PromptTemplate(input_variables=["context", "question"], template=template)
+system_prompt = (
+    "Use the given context to answer the question. "
+    "If you don't know the answer, say you don't know. "
+    "Use three sentence maximum and keep the answer concise. "
+    "Context: {context}"
+)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+# build chain
+retriever = vector_db.as_retriever()
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+chain = create_retrieval_chain(retriever, question_answer_chain)
 
 # run chain
-question = "What is the CAP theorem?"
-qa_chain = RetrievalQA.from_chain_type(llm,
-                                       retriever=vector_db.as_retriever(),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={"prompt": qa_chain_prompt})
-
-print("Running AI...\n")
-result = qa_chain.invoke({"query": question})
-print(result["result"])
+question = query
+print("> running lang chain and ask:", question, "\n")
+result = chain.invoke({"input": question})
+print(result['answer'])
