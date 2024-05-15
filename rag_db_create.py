8a38130d11845bda46546791b24ba645e89204f9
Neo Lee <neo.lee@gmail.com>
2024-05-15 16:45:44 +0800

    feat: more books to feed in chroma

--- rag_db_create.py
+++ rag_db_create.py
@@ -6,13 +6,20 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 
-loaders = [PyPDFLoader('./books/cap.pdf')]
-
+# load pdf text
+pdfs = ['./books/cap.pdf',
+        './books/feynman-cn.pdf',
+        './books/illiberal-democracy.pdf',
+        './books/mysterious-cases-cn.pdf',
+        './books/sicp.pdf']
 docs = []
-for file in loaders:
-    docs.extend(file.load())
+for file in pdfs:
+    print('loading', file)
+    loader = PyPDFLoader(file)
+    docs.extend(loader.load())
 
 # split text to chunks
+print('splitting text to chunks')
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(docs)
 
@@ -20,4 +27,6 @@ docs = text_splitter.split_documents(docs)
 embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # load it into chroma
+print('writing into chroma db')
 vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
+print('done')
